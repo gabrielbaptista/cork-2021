e40d15d49441c05363ff5aee4ad5f1860ef60367
gabriell <gabriel@smit.net.br>
2021-03-23 21:21:23 -0300

    Cork Web App First Version

---
+++ WebAppSample/WebAppSample/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppSample.Data;
+
+[assembly: HostingStartup(typeof(WebAppSample.Areas.Identity.IdentityHostingStartup))]
+namespace WebAppSample.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
