50ee199aefdf31a654f2e064d9742a0957b5b7df
gabriell <gabriel@smit.net.br>
2021-03-28 13:10:39 -0300

    Small changes to monitor

--- WebAppSample/Controllers/HomeController.cs
+++ WebAppSample/Controllers/HomeController.cs
@@ -33,5 +33,11 @@ namespace WebAppSample.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult CreateError()
+        {
+            return View();
+        }
+
     }
 }
